ee2ecda590d622c0e5406e2648cd6e1b7a9d6347
Vinamra <agarwal.vinamra0405@gmail.com>
2025-06-23 14:35:28 +0530

    setting up dashboard page

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,17 @@
-import * as z from 'zod';
+import * as z from "zod";
 
 export const FormSchema = z.object({
-  email: z.string().describe('Email').email({message: 'Invalid Email'}),
-  password: z.string().describe('Password').min(1, {message: 'Password is required'}),
+  email: z.string().describe("Email").email({ message: "Invalid Email" }),
+  password: z
+    .string()
+    .describe("Password")
+    .min(1, { message: "Password is required" }),
+});
+
+export const CreateWorkspaceFormSchema = z.object({
+  workspaceName: z
+    .string()
+    .describe("Workspace Name")
+    .min(1, { message: "Workspace Name must be min of 1 character" }),
+  logo: z.any(),
 });
